648ff257b84a0ef59f777bda8fed06ecebdd4065
SIKU-KR <peter012677@naver.com>
2024-05-01 15:48:16 +0900

    [1.3.1] Minor Changes 데이터베이스 관련 로직 최적화 작업

--- app/src/main/java/com/example/holybean/common/DatabaseManager.kt
+++ app/src/main/java/com/example/holybean/common/DatabaseManager.kt
@@ -13,9 +13,7 @@ import com.example.holybean.dataclass.OrderItem
 import com.example.holybean.dataclass.OrdersDetailItem
 import com.example.holybean.dataclass.ReportDetailItem
 import com.opencsv.CSVReaderBuilder
-import java.io.BufferedReader
 import java.io.FileOutputStream
-import java.io.FileReader
 import java.io.InputStreamReader
 import java.text.SimpleDateFormat
 import java.util.Date
@@ -27,6 +25,37 @@ class DatabaseManager private constructor(
     companion object {
         private const val DATABASE_NAME = "database.db"
         private const val DATABASE_VERSION = 1
+
+        // 테이블 정의
+        // 테이블 이름 정의
+        const val ORDERS = "Orders"
+        const val CREDITS = "Credits"
+        const val DETAILS = "Details"
+
+        // Orders 테이블
+        const val ORDERS_ID = "id"
+        const val ORDERS_ORDER_ID = "order_id"
+        const val ORDERS_DATE = "order_date"
+        const val ORDERS_TOTAL_AMOUNT = "total_amount"
+        const val ORDERS_METHOD = "method"
+        const val ORDERS_ORDERER = "orderer"
+
+        // Credits 테이블
+        const val CREDITS_ID = "id"
+        const val CREDITS_ORDER_ID = "order_id"
+        const val CREDITS_DATE = "order_date"
+        const val CREDITS_TOTAL_AMOUNT = "total_amount"
+        const val CREDITS_ORDERER = "orderer"
+
+        // Details 테이블 컬럼
+        const val DETAILS_ID = "id"
+        const val DETAILS_ORDER_ID = "order_id"
+        const val DETAILS_DATE = "date"
+        const val DETAILS_PRODUCT_ID = "product_id"
+        const val DETAILS_PRODUCT_NAME = "product_name"
+        const val DETAILS_QUANTITY = "quantity"
+        const val DETAILS_PRICE = "price"
+        const val DETAILS_SUBTOTAL = "subtotal"
 
         private var instance: DatabaseManager? = null
 
@@ -95,49 +124,54 @@ class DatabaseManager private constructor(
             val currentDate = instance.getCurrentDate()
             var lastorderId: Int? = null
             val db = instance.readableDatabase
-            val query = "SELECT MAX(order_id) FROM Orders WHERE order_date = ?"
+            val query = "SELECT MAX($ORDERS_ORDER_ID) FROM $ORDERS WHERE $ORDERS_DATE = ?"
             val cursor: Cursor = db.rawQuery(query, arrayOf(currentDate))
-
             cursor.use {
                 if (it.moveToFirst()) {
                     lastorderId = it.getInt(0)
                 }
             }
-            cursor.close()
             db.close()
             return lastorderId?.plus(1) ?: 1
         }
 
-
         fun orderProcess(context: Context, orderId: Int, totalPrice: Int, orderMethod: String, ordererName: String){
             val dbHelper = getInstance(context)
             val currentDate = dbHelper.getCurrentDate()
-            val ordersDb = dbHelper.writableDatabase
+            val db = dbHelper.writableDatabase
 
-            // Insert into Orders table
-            val ordersValues = ContentValues().apply {
-                put("order_id", orderId)
-                put("order_date", currentDate)
-                put("total_amount", totalPrice)
-                put("method", orderMethod)
-                put("orderer", ordererName)
-            }
-            ordersDb.insert("Orders", null, ordersValues)
-
-            if(orderMethod == "외상"){
-                val creditDb = dbHelper.writableDatabase
-                val creditValues = ContentValues().apply {
-                    put("order_id", orderId)
-                    put("order_date", currentDate)
-                    put("total_amount", totalPrice)
-                    put("orderer", ordererName)
+            try {
+                // db 트랜잭션 시작
+                db.beginTransaction()
+                // Orders 테이블에는 항상 Insert
+                val ordersValues = ContentValues().apply {
+                    put(ORDERS_ORDER_ID, orderId)
+                    put(ORDERS_DATE, currentDate)
+                    put(ORDERS_TOTAL_AMOUNT, totalPrice)
+                    put(ORDERS_METHOD, orderMethod)
+                    put(ORDERS_ORDERER, ordererName)
                 }
-                ordersDb.insert("Credits", null, creditValues)
-                creditDb.close()
+                db.insert(ORDERS, null, ordersValues)
+                // Credits 테이블에는 외상일때만 Insert
+                if(orderMethod == "외상"){
+                    val creditValues = ContentValues().apply {
+                        put(CREDITS_ORDER_ID, orderId)
+                        put(CREDITS_DATE, currentDate)
+                        put(CREDITS_TOTAL_AMOUNT, totalPrice)
+                        put(CREDITS_ORDERER, ordererName)
+                    }
+                    db.insert(CREDITS, null, creditValues)
+                }
+                // 트랜잭션 종류
+                db.setTransactionSuccessful()
+            } finally {
+                db.endTransaction()
             }
-
-            // Close the databases after use
-            ordersDb.close()
+            db.close()
+            /*
+            * 함수 개선 사항 : 실패하면 롤백되는데, 재시도하여서 DB에 정상작동할 방법을 적용하지 않았다.
+            * 나중에 서버로 데이터를 옮기게 된다면 HTTP로 만들어서 관련 처리를 방안을 고려해보면 좋을듯.
+            */
         }
 
         fun orderDetailProcess(context: Context, orderId: Int, basket: ArrayList<BasketItem>) {
@@ -148,17 +182,21 @@ class DatabaseManager private constructor(
             // Insert into Details table
             for (basketItem in basket) {
                 val detailsValues = ContentValues().apply {
-                    put("order_id", orderId)
-                    put("date",currentDate)
-                    put("product_id", basketItem.id)
-                    put("product_name", basketItem.name)
-                    put("quantity", basketItem.count)
-                    put("price", basketItem.price)
-                    put("subtotal", basketItem.total)
+                    put(DETAILS_ORDER_ID, orderId)
+                    put(DETAILS_DATE,currentDate)
+                    put(DETAILS_PRODUCT_ID, basketItem.id)
+                    put(DETAILS_PRODUCT_NAME, basketItem.name)
+                    put(DETAILS_QUANTITY, basketItem.count)
+                    put(DETAILS_PRICE, basketItem.price)
+                    put(DETAILS_SUBTOTAL, basketItem.total)
                 }
-                detailsDb.insert("Details", null, detailsValues)
+                detailsDb.insert(DETAILS, null, detailsValues)
             }
             detailsDb.close()
+
+            /*
+            * 이 함수는 나중에 서버화 시킬 때, 위에 Order Process와 하나의 함수로 만들어서 트랜잭션의 성능을 확실히 하자.
+            */
         }
 
         fun deleteCreditRecord(context: Context, orderNum: Int, orderDate: String){
@@ -167,16 +205,19 @@ class DatabaseManager private constructor(
 
             try {
                 // Delete from Credits table
-                val whereClause = "order_id = ? AND order_date = ?"
+                val whereClause = "$CREDITS_ORDER_ID = ? AND $CREDITS_DATE = ?"
                 val whereArgs = arrayOf(orderNum.toString(), orderDate)
-                creditDb.delete("Credits", whereClause, whereArgs)
-                println("Credit record deleted successfully.")
+                creditDb.delete(CREDITS, whereClause, whereArgs)
+                // println("Credit record deleted successfully.")
             } catch (e: Exception) {
                 e.printStackTrace()
             } finally {
                 // Close the database after use
                 creditDb.close()
             }
+            /*
+            * 효율적으로 try - catch 문을 쓰자..
+            */
         }
 
         fun deleteAnOrder(context: Context, orderNum: Int, orderDate: String){
@@ -185,14 +226,14 @@ class DatabaseManager private constructor(
 
             try {
                 // Delete from Orders table
-                val whereClause1 = "order_id = ? AND order_date = ?"
+                val whereClause1 = "$ORDERS_ORDER_ID = ? AND $ORDERS_DATE = ?"
                 val whereArgs1 = arrayOf(orderNum.toString(), orderDate)
-                db.delete("Orders", whereClause1, whereArgs1)
+                db.delete(ORDERS, whereClause1, whereArgs1)
 
                 // Delete from Details table
-                val whereClause2 = "order_id = ? AND date = ?"
+                val whereClause2 = "$DETAILS_ORDER_ID = ? AND $DETAILS_DATE = ?"
                 val whereArgs2 = arrayOf(orderNum.toString(), orderDate)
-                db.delete("Details", whereClause2, whereArgs2)
+                db.delete(DETAILS, whereClause2, whereArgs2)
 
             } catch (e: Exception) {
                 e.printStackTrace()
@@ -229,19 +270,17 @@ class DatabaseManager private constructor(
     private fun readOrders(date: String): ArrayList<OrderItem> {
         val orderList = ArrayList<OrderItem>()
         val db = this.readableDatabase
-        val cursor: Cursor = db.rawQuery("SELECT * FROM Orders WHERE order_date = ?", arrayOf(date))
+        val cursor: Cursor = db.rawQuery("SELECT * FROM $ORDERS WHERE $ORDERS_DATE = ? ORDER BY $ORDERS_ORDER_ID", arrayOf(date))
         cursor.use {
             while (it.moveToNext()) {
-                val orderId = it.getInt(it.getColumnIndex("order_id"))
-                val totalAmount = it.getInt(it.getColumnIndex("total_amount"))
-                val method = it.getString(it.getColumnIndex("method"))
-                val orderer = it.getString(it.getColumnIndex("orderer"))
+                val orderId = it.getInt(it.getColumnIndex(ORDERS_ORDER_ID))
+                val totalAmount = it.getInt(it.getColumnIndex(ORDERS_TOTAL_AMOUNT))
+                val method = it.getString(it.getColumnIndex(ORDERS_METHOD))
+                val orderer = it.getString(it.getColumnIndex(ORDERS_ORDERER))
                 orderList.add(OrderItem(orderId, totalAmount, method, orderer))
             }
         }
-        cursor.close()
         db.close()
-        orderList.sortBy { it.orderId }
         return orderList
     }
 
@@ -249,19 +288,17 @@ class DatabaseManager private constructor(
     private fun readCredits(): ArrayList<CreditItem> {
         val orderList = ArrayList<CreditItem>()
         val db = this.readableDatabase
-        val cursor: Cursor = db.rawQuery("SELECT * FROM Credits", null)
+        val cursor: Cursor = db.rawQuery("SELECT * FROM $CREDITS ORDER BY $CREDITS_DATE", null)
         cursor.use {
             while (it.moveToNext()) {
-                val orderId = it.getInt(it.getColumnIndex("order_id"))
-                val totalAmount = it.getInt(it.getColumnIndex("total_amount"))
-                val orderDate = it.getString(it.getColumnIndex("order_date"))
-                val orderer = it.getString(it.getColumnIndex("orderer"))
+                val orderId = it.getInt(it.getColumnIndex(CREDITS_ORDER_ID))
+                val totalAmount = it.getInt(it.getColumnIndex(CREDITS_TOTAL_AMOUNT))
+                val orderDate = it.getString(it.getColumnIndex(CREDITS_DATE))
+                val orderer = it.getString(it.getColumnIndex(CREDITS_ORDERER))
                 orderList.add(CreditItem(orderId, totalAmount, orderDate, orderer))
             }
         }
-        cursor.close()
         db.close()
-        orderList.sortBy { it.date }
         return orderList
     }
 
@@ -305,39 +342,29 @@ class DatabaseManager private constructor(
 
     @SuppressLint("Range")
     private fun makeReportDetail(date1: String, date2: String): ArrayList<ReportDetailItem> {
-        var result = mutableMapOf<Int, Triple<String, Int, Int>>()
+        val resultList = ArrayList<ReportDetailItem>()
         val db = this.readableDatabase
-        val cursor: Cursor = db.rawQuery("SELECT * FROM Details WHERE date BETWEEN ? AND ?",arrayOf(date1, date2))
+        val query = """
+        SELECT MIN($DETAILS_PRODUCT_ID) AS min_id, $DETAILS_PRODUCT_NAME, SUM($DETAILS_QUANTITY) AS total_quantity, SUM($DETAILS_SUBTOTAL) AS total_subtotal
+        FROM $DETAILS
+        WHERE $DETAILS_DATE BETWEEN ? AND ?
+        GROUP BY $DETAILS_PRODUCT_ID, $DETAILS_PRODUCT_NAME
+        ORDER BY min_id;
+        """.trimIndent()
+        val cursor: Cursor = db.rawQuery(query, arrayOf(date1, date2))
+
         cursor.use {
             while (it.moveToNext()) {
-                val productId = it.getInt(it.getColumnIndex("product_id"))
-                val productName = it.getString(it.getColumnIndex("product_name"))
-                val quantity = it.getInt(it.getColumnIndex("quantity"))
-                val subtotal = it.getInt(it.getColumnIndex("subtotal"))
-
-                // Check if the productName is already in the map
-                if (result.containsKey(productId)) {
-                    // If yes, update the existing entry by adding the current quantity and subtotal
-                    val currentTriple = result[productId]!!
-                    val updatedTriple = Triple(productName, currentTriple.second + quantity, currentTriple.third + subtotal)
-                    result[productId] = updatedTriple
-                } else {
-                    // If no, create a new entry with the current quantity and subtotal
-                    result[productId] = Triple(productName, quantity, subtotal)
-                }
+                val productId = it.getInt(it.getColumnIndex("min_id"))
+                val productName = it.getString(it.getColumnIndex(DETAILS_PRODUCT_NAME))
+                val totalQuantity = it.getInt(it.getColumnIndex("total_quantity"))
+                val totalSubtotal = it.getInt(it.getColumnIndex("total_subtotal"))
+                resultList.add(ReportDetailItem(productId, productName, totalQuantity, totalSubtotal))
             }
         }
         db.close()
-
-        val menuData = getMenuList().filter {it.price == 0}
-        val filterData = menuData.map { it.id }.toSet()
-        val filteredResult = result.filterKeys { it !in filterData }.toMutableMap()
-
-        val reportDetailsList = ArrayList(filteredResult.entries.map { entry ->
-            ReportDetailItem(entry.key, entry.value.first, entry.value.second, entry.value.third)
-        }.sortedBy { reportDetail -> reportDetail.id })
-
-        return reportDetailsList
+        resultList.removeIf { it.subtotal == 0 }
+        return resultList
     }
 
 }
