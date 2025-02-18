c7943858ea0611a42913473f020df9c7045f1c0e
SIKU-KR <peter012677@naver.com>
2025-02-18 15:38:06 +0900

    Bug Fix
    1. 영수증에 null로 출력되는 경우 수정

--- app/src/main/java/com/example/holybean/printer/ReportPrinter.kt
+++ app/src/main/java/com/example/holybean/printer/ReportPrinter.kt
@@ -4,16 +4,18 @@ import com.example.holybean.data.model.PrinterDTO
 
 class ReportPrinter: Printer() {
 
-    fun getPrintingText(data: PrinterDTO): String{
+    fun getPrintingText(data: PrinterDTO): String {
         var result = "[L]\n"
         result += "[C]<u><font size='big'>${data.startdate}~</font></u>\n"
         result += "[C]<u><font size='big'>${data.enddate}</font></u>\n"
         result += "[C]-------------------------------------\n"
-        result += "[L]총 판매금액 : ${data.reportData["전체"]}\n"
-        result += "[L]현금 판매금액 : ${data.reportData["현금"]}\n"
-        result += "[L]쿠폰 판매금액 : ${data.reportData["쿠폰"]}\n"
-        result += "[L]계좌이체 판매금액 : ${data.reportData["계좌이체"]}\n"
-        result += "[L]외상 판매금액 : ${data.reportData["외상"]}\n"
+        result += "[L]총 판매금액 : ${data.reportData["총합"] ?: 0}\n"
+        result += "[L]현금 판매금액 : ${data.reportData["현금"] ?: 0}\n"
+        result += "[L]쿠폰 판매금액 : ${data.reportData["쿠폰"] ?: 0}\n"
+        result += "[L]계좌이체 판매금액 : ${data.reportData["계좌이체"] ?: 0}\n"
+        result += "[L]외상 판매금액 : ${data.reportData["외상"] ?: 0}\n"
+        result += "[L]무료쿠폰 판매금액 : ${data.reportData["무료쿠폰"] ?: 0}\n"
+        result += "[L]무료제공 판매금액 : ${data.reportData["무료제공"] ?: 0}\n"
         result += "[C]-------------------------------------\n"
         result += "[L]이름[R]수량[R]판매액\n"
         for(item in data.reportDetailItem) {
@@ -22,5 +24,6 @@ class ReportPrinter: Printer() {
         result += "[L]\n"
         return result
     }
+
 
 }
