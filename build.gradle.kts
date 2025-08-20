73b853919bc46ee9ecd16c71654a6fd1a436e1f5
Bumshik Park <peter012677@naver.com>
2025-08-20 13:32:33 +0900

    feat: 프로젝트 의존성에 KSP와 ROOM 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,16 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.52")
+    }
+}
+
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.23" apply false
     id("com.google.dagger.hilt.android") version "2.52" apply false
+    id("com.google.devtools.ksp") version "1.9.23-1.0.19" apply false
 }
