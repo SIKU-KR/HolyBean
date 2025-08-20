95f51ee31b556523bdc73d56a5c8cce019a71a41
Bumshik Park <peter012677@naver.com>
2025-08-20 13:34:56 +0900

    feat: MenuItem을 Room의 Entity로 변경

--- app/src/main/java/eloom/holybean/data/model/MenuItem.kt
+++ app/src/main/java/eloom/holybean/data/model/MenuItem.kt
@@ -1,9 +1,24 @@
 package eloom.holybean.data.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "menu")
 data class MenuItem(
+    @PrimaryKey
+    @ColumnInfo(name = "id")
     val id: Int,
-    var name: String,
-    var price: Int,
-    var order: Int,
-    var inuse: Boolean
+
+    @ColumnInfo(name = "name")
+    val name: String,
+
+    @ColumnInfo(name = "price")
+    val price: Int,
+
+    @ColumnInfo(name = "placement")
+    val order: Int,
+
+    @ColumnInfo(name = "inuse")
+    val inuse: Boolean
 )
