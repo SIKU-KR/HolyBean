c154cb9b61dd0dfabcafc0c0e573a2ddc7f073b6
Bumshik Park <peter012677@naver.com>
2025-09-23 14:54:10 +0900

    Refactors Printer to manage Bluetooth connection lifecycle and error handling

--- app/src/main/java/eloom/holybean/printer/Printer.kt
+++ app/src/main/java/eloom/holybean/printer/Printer.kt
@@ -2,27 +2,36 @@ package eloom.holybean.printer
 
 import eloom.holybean.escpos.EscPosCharsetEncoding
 import eloom.holybean.escpos.EscPosPrinter
+import eloom.holybean.escpos.connection.PrinterConnectionException
+import eloom.holybean.escpos.connection.bluetooth.BluetoothConnection
 import eloom.holybean.escpos.connection.bluetooth.BluetoothPrintersConnections
 
-abstract class Printer {
+abstract class Printer(
+    private val bluetoothPrintersConnections: BluetoothPrintersConnections,
+) {
 
-    private val printer: EscPosPrinter
-
-    init {
-        printer = EscPosPrinter(
-            BluetoothPrintersConnections.selectFirstPaired(),
-            180,
-            72f,
-            32,
-            EscPosCharsetEncoding("EUC-KR", 13)
-        )
-    }
+    private val encoding = EscPosCharsetEncoding("EUC-KR", 13)
+    private var activeConnection: BluetoothConnection? = null
 
     fun print(data: String) {
-        printer.printFormattedTextAndCut(data, 500)
+        val connection = bluetoothPrintersConnections.getList().firstOrNull()
+            ?: throw PrinterConnectionException.BluetoothUnavailable
+
+        connection.connect()
+        activeConnection = connection
+
+        val printer = EscPosPrinter(connection, 180, 72f, 32, encoding)
+
+        try {
+            printer.printFormattedTextAndCut(data, 500)
+        } finally {
+            printer.disconnectPrinter()
+            activeConnection = null
+        }
     }
 
     fun disconnect() {
-        printer.disconnectPrinter()
+        activeConnection?.disconnect()
+        activeConnection = null
     }
 }
