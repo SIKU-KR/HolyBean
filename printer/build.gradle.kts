44b2c8b4e90ac5ece791535cc0245fae59b6fba0
Bumshik Park <peter012677@naver.com>
2025-09-23 09:18:44 +0900

    Add EscPosPrinterCommands class for ESC/POS command management.

---
+++ printer/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("com.android.library")
+}
+
+android {
+    namespace = "eloom.holybean.escpos"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 26
+    }
+
+    buildTypes {
+        debug { }
+        release {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.6.1")
+}
