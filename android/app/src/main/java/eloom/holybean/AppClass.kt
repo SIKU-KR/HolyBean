57d79268965d4046855184983bb26ea3fc65e62b
Benn <benn@daangn.com>
2026-05-23 22:18:35 +0900

    feat: init firebase app check and anonymous auth

--- android/app/src/main/java/eloom/holybean/AppClass.kt
+++ android/app/src/main/java/eloom/holybean/AppClass.kt
@@ -1,7 +1,25 @@
 package eloom.holybean
 
 import android.app.Application
+import com.google.firebase.appcheck.appCheck
+import com.google.firebase.appcheck.debug.DebugAppCheckProviderFactory
+import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory
+import com.google.firebase.auth.auth
+import com.google.firebase.Firebase
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class AppClass : Application()
+class AppClass : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        Firebase.appCheck.installAppCheckProviderFactory(
+            if (BuildConfig.DEBUG) DebugAppCheckProviderFactory.getInstance()
+            else PlayIntegrityAppCheckProviderFactory.getInstance()
+        )
+
+        if (Firebase.auth.currentUser == null) {
+            Firebase.auth.signInAnonymously()
+        }
+    }
+}
