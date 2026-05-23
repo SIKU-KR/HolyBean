80af3e208c6619b4d9d7fb906d70ab81dc28ed28
Benn <benn@daangn.com>
2026-05-24 00:22:36 +0900

    Upgrade AGP dependency from 8.11.2 to 8.13.2

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "8.11.2" apply false
+    id("com.android.application") version "8.13.2" apply false
     id("org.jetbrains.kotlin.android") version "2.2.20" apply false
     id("com.google.dagger.hilt.android") version "2.57.1" apply false
     id("com.google.devtools.ksp") version "2.2.20-2.0.2" apply false
