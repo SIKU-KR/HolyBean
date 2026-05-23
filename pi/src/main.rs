582e7bfdda4d2d0e99b1d40319a5cfba6eab8e46
Benn <benn@daangn.com>
2026-05-23 16:19:38 +0900

    feat(pi): add /print and /health HTTP handlers

--- pi/src/main.rs
+++ pi/src/main.rs
@@ -1,6 +1,7 @@
 mod command;
 mod error;
 mod escpos;
+mod http;
 mod layout;
 mod queue;
 mod sink;
@@ -8,6 +9,7 @@ mod sink;
 pub use command::*;
 pub use error::*;
 pub use escpos::*;
+pub use http::*;
 pub use layout::*;
 pub use queue::*;
 pub use sink::*;
