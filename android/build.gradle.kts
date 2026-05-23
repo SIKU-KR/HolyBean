1db64d35b1fec0467a61a3b8bf90d8406ce6109d
Benn <benn@daangn.com>
2026-05-23 22:16:47 +0900

    build: add firebase gradle plugins to classpath

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -13,4 +13,6 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.2.20" apply false
     id("com.google.dagger.hilt.android") version "2.57.1" apply false
     id("com.google.devtools.ksp") version "2.2.20-2.0.2" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.google.firebase.crashlytics") version "3.0.2" apply false
 }
