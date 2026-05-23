d3cc7e7a33298a11bd91bebf3ba1b6ecfc139b18
Benn <benn@daangn.com>
2026-05-24 02:17:08 +0900

    feat(ui): SegmentedToggle solid-orange selection with dark text

--- android/app/src/main/java/eloom/holybean/ui/components/SegmentedToggle.kt
+++ android/app/src/main/java/eloom/holybean/ui/components/SegmentedToggle.kt
@@ -3,7 +3,10 @@ package eloom.holybean.ui.components
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import eloom.holybean.ui.theme.Orange
+import eloom.holybean.ui.theme.OnSurface
+import eloom.holybean.ui.theme.OnSurfaceMuted
 import kotlinx.collections.immutable.ImmutableList
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -12,8 +15,15 @@ fun SegmentedToggle(options: ImmutableList<String>, selected: String, onSelect: 
     SingleChoiceSegmentedButtonRow {
         options.forEachIndexed { i, opt ->
             SegmentedButton(
-                selected = opt == selected, onClick = { onSelect(opt) },
+                selected = opt == selected,
+                onClick = { onSelect(opt) },
                 shape = SegmentedButtonDefaults.itemShape(i, options.size),
+                colors = SegmentedButtonDefaults.colors(
+                    activeContainerColor = Orange,
+                    activeContentColor = OnSurface,
+                    inactiveContainerColor = Color(0xFFF0F0F0),
+                    inactiveContentColor = OnSurfaceMuted,
+                ),
             ) { Text(opt) }
         }
     }
